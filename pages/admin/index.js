aaed36adc59aba45c762b55b3539498134cbc5a6
chrsjonzz <chrsjonzz74@gmail.com>
2025-04-18 09:40:28 +0530

    Fix: use relative import for Admin.module.css

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from '@/styles/Admin.module.css';
+import styles from '../../styles/Admin.module.css'
 
 export default function Admin() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
